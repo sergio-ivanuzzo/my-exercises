a7ba6ddd09d607bcab024d72e22da3cde703ad63
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2024-08-19 19:01:33 +0200

    currency converter

--- src/CurrencyConverter/index.tsx
+++ src/CurrencyConverter/index.tsx
@@ -1,26 +1,56 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
+import useDebounce from "../Proj0/hooks";
+import styles from "./index.module.css";
 
-const K = 1.1;
+const METADATA: { [key: string]: number } = {
+    "EUR_USD": 1.1,
+    "USD_EUR": 0.91,
+}
 
 const CurrencyConverter = () => {
-    const [from, setFrom] = useState(0.0);
-    const [to, setTo] = useState(0.0);
+    const [data, setData] = useState({ from: "EUR", value: 0, to: "USD" });
+    const [inputValue, setInputValue] = useState("");
+    const [isDirty, markDirty] = useState(false);
 
-    useEffect(() => {
-        setTo(K * from);
-    }, [from]);
+    const debouncedInputChange = useDebounce((value: string) => {
+        markDirty(true);
+        setData({ ...data, value: parseFloat(value) });
+    }, 250);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        debouncedInputChange(e.target.value);
+        setInputValue(e.target.value);
+    };
+
+    const handleExchange = () => {
+        markDirty(false);
+        setInputValue("");
+    };
+
+    const handleSwitch = () => {
+        if (data.from === "EUR") {
+            setData({ ...data, from: "USD", to: "EUR" });
+        } else {
+            setData({ ...data, from: "EUR", to: "USD" });
+        }
+    };
+
+    const showValue = !isDirty && !!data.value;
 
     return (
-        <>
+        <div className={styles.container}>
             <label>
-                <span>USD</span>
-                <input type="text" value={from} onChange={(e) => setFrom(+e.target.value)}/>
+                <span>{data.from}</span>
+                <input type="text" value={inputValue} onChange={handleInputChange}/>
             </label>
-            <label>
-                <span>EUR</span>
-                <input type="text" value={to}/>
-            </label>
-        </>
+            <button onClick={handleSwitch}>{`${data.from} TO ${data.to}`}</button>
+            <button onClick={handleExchange}>Exchange</button>
+            {showValue && (
+                <div className={styles.value}>
+                    {data.value * METADATA[`${data.from}_${data.to}`]}
+                </div>
+            )}
+        </div>
     );
 };
 
